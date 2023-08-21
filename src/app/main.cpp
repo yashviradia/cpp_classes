7e1ed5dcc6a88f037acac42cd4ec90606ff00f27
yashviradia <y.viradia@tu-braunschweig.de>
2023-08-21 22:29:21 +0200

    used Template class

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,26 +2,18 @@
 using std::cout;
 using std::endl;
 
-#include <vector>
-using std::vector;
+#include <string>
+using std::string;
 
-#include <exception>
+#include "Accumulator.h"
 
 int main() {
-    vector v = {1, 2};
 
-    // Output of this exception is not that helpful.
-    // But it does help in knowing there's an exception.
-    try {
-        v.at(3);
-    }
-    catch (std::exception const & e) {
-        cout << e.what() << endl;
-    }
-
-    catch(...) {
-        cout << "An uncaught exception occured." << endl;
-    }
+    Accumulator<int> acc(0);
+    acc.add(1);
+    acc.add(9);
+    cout << acc.getValue() << endl;
+    
 
     return 0;
 }
