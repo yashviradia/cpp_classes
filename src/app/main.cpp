1de364f07e1cfb7ae17f1a62d6fa8c964dfa448a
yashviradia <y.viradia@tu-braunschweig.de>
2023-08-24 12:12:49 +0200

    completed lambda timer

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::endl;
 
-class Functor {
-public:
-    int x;
-    int operator() (int val) { return x + val; };
-};
+template<typename Func>
+long long timeIt(Func f)
+{
+    auto begin = std::chrono::system_clock::now();
+
+    f();
+
+    auto end = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+}
+
+void createVector(std::vector<int> & result, int numItems) {
+    for (int i = 0; i < numItems; i++) {
+        result.push_back(i);
+    }
+}
 
 int main() {
+    using namespace std;
 
-    auto lambda = [](int val){ return val; };
+    vector<int> v1;
+    vector<int> v2;
 
-    Functor func;
-    func.x = 5;
+    auto lambda1 = [&](){ createVector(v1, 100); };
+    auto lambda2 = [&](){ createVector(v2, 1000000); };
 
-    cout << func(7) << endl;
+    cout << "100:      " << timeIt(lambda1) << endl;
+    cout << "1000000:  " << timeIt(lambda2) << endl;
 
     return 0;
 }
