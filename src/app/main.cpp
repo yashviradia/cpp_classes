9129e05c3bb05c7ad290de98cf8ff2f3bb1be634
yashviradia <y.viradia@tu-braunschweig.de>
2023-08-25 14:29:50 +0200

    question 3

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,24 +1,19 @@
-#include <vector>
 #include <iostream>
-#include <numeric>
-#include <vector>
+
+float getPI() {
+    return 3.14f;
+}
+
+float cylinder_volume() {
+    auto pi = getPI();
+
+    auto radius = 15;
+
+    auto height = 9.265;
+
+    return static_cast<float>(pi * radius * radius * height);
+}
 
 int main() {
-    std::vector<int> vec(10);
-
-    std::iota(vec.begin(), vec.end(), 1);
-
-    std::transform(vec.begin(), vec.end(), vec.begin(),
-                   [](int val){return val * val; });
-
-    for (std::size_t i = 0; i < vec.size(); i++) {
-        vec[i] = vec[vec.size() - i - 1];
-    };
-
-    for (auto const v : vec) {
-        std::cout << v << " ";
-    }
-    std::cout << std::endl;
-
-
+    std::cout << cylinder_volume() << std::endl;
 }
